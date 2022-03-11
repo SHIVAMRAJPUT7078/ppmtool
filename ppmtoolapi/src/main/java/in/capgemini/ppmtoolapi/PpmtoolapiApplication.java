68e1c39ad4f19c1d5709c11c5d4320cefd94018b
shiva <shivam.rajput_cs18@gla.ac.in>
2022-03-11 10:37:02 +0530

    init

---
+++ ppmtoolapi/src/main/java/in/capgemini/ppmtoolapi/PpmtoolapiApplication.java
@@ -0,0 +1,14 @@
+package in.capgemini.ppmtoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolapiApplication.class, args);
+		
+	}
+
+}
